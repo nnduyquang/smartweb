7764f7763c732520f0cfe0f21965c9aaecb3de3e
loiPH <loiph.vncoder@users.noreply.github.com>
2018-08-07 10:18:09 +0700

    ceo

--- local/resources/views/Frontsend/Home/fouder.blade.php
+++ local/resources/views/Frontsend/Home/fouder.blade.php
@@ -130,7 +130,7 @@
             <div class="col-lg-3 text-center col-md-6 pb-3 col-12 d-flex align-items-center justify-content-center">
                 <i class="fas fa-users fa-2x text-white"></i>
                 <div class="counter_area d-flex flex-column justify-content-center pl-3">
-                    <span id="counter-number1" class="h_static" data-from="1" data-to="150" data-speed="1000">
+                    <span id="counter-number1" data-from="1" data-to="150" data-speed="1000">
                         </span>
                     <span class="counter-text p-0 m-0">Members</span>
                 </div>
@@ -139,7 +139,7 @@
             <div class="col-lg-3 text-center col-md-6 pb-3 col-12 d-flex align-items-center justify-content-center">
                 <i class="fas fa-university fa-2x text-white"></i>
                 <div class="counter_area d-flex flex-column justify-content-center pl-3">
-                    <span id="counter-number2" class="h_static" data-from="1" data-to="150" data-speed="1000">
+                    <span id="counter-number2" data-from="1" data-to="150" data-speed="1000">
                         </span>
                     <span class="counter-text p-0 m-0">Experience</span>
                 </div>
@@ -148,7 +148,7 @@
             <div class="col-lg-3 text-center col-md-6 pb-3 col-12 d-flex align-items-center justify-content-center">
                 <i class="fas fa-laptop fa-2x text-white"></i>
                 <div class="counter_area d-flex flex-column justify-content-center pl-3">
-                    <span id="counter-number3" class="h_static" data-from="1" data-to="150" data-speed="1000">
+                    <span id="counter-number3" data-from="1" data-to="150" data-speed="1000">
                         </span>
                     <span class="counter-text p-0 m-0">Projects</span>
                 </div>
@@ -157,7 +157,7 @@
             <div class="col-lg-3 text-center col-md-6 pb-3 col-12 d-flex align-items-center justify-content-center">
                 <i class="fas fa-trophy fa-2x text-white"></i>
                 <div class="counter_area d-flex flex-column justify-content-center pl-3">
-                    <span id="counter-number4" class="h_static" data-from="1" data-to="150" data-speed="1000">
+                    <span id="counter-number4" data-from="1" data-to="150" data-speed="1000">
                         </span>
                     <span class="counter-text p-0 m-0">Award</span>
                 </div>
